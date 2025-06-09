0a16e77055ec2b018c4de64cfd46669ecd9badec
sharon caroline <you@example.com>
2025-06-09 12:28:36 +0300

    Complete employee object functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// employee.js
+
+const employee = {
+  name: "Sharon Anyango",
+  streetAddress: "123 Web Dev Lane"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
